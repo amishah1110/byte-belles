9c0f1f4c6e02a6dedd133b924bbd8f387949dc74
Ami Shah <mailami.shah@gmail.com>
2025-04-03 10:31:07 +0530

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,10 @@
+#pip install pandas
+#pip install openpyxl
+import pandas as pd
+
+# Load CSV
+df = pd.read_excel("D:\\Users\\amisa\\Downloads\\Social Media Usage - Train.xlsm")
+
+# Check data
+print(df.head())
+
